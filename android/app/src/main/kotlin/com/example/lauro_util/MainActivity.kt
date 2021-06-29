b8b23906d5695c78acfc5eff668be0be184df0b0
wasgton <wasgton.it@gmail.com>
2021-06-29 20:07:16 -0300

    Estrutura do projeto

---
+++ android/app/src/main/kotlin/com/example/lauro_util/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lauro_util
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:lauro_util/pages/home/home.dart';
+
+void main() {
+  runApp(MaterialApp(home:Home()));
+}
